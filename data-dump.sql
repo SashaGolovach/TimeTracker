e15df2cdac482503fe546db6baebec984b9ec5c7
Sasha Golovach <sashagolovach@Sashas-MacBook-Pro.local>
2018-10-12 09:50:49 +0300

    .

--- data-dump.sql
+++ data-dump.sql
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
+SET NAMES utf8mb4 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,13 +21,18 @@
 
 DROP TABLE IF EXISTS `Actions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `Actions` (
-  `task_id` int(11) NOT NULL,
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `started` varchar(45) DEFAULT NULL,
-  `ended` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Actions`
+(
+  `task_id` int
+(11) NOT NULL,
+  `id` int
+(10) unsigned NOT NULL AUTO_INCREMENT,
+  `started` varchar
+(45) DEFAULT NULL,
+  `ended` varchar
+(45) DEFAULT NULL,
+  PRIMARY KEY
+(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -46,13 +51,17 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Tasks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `Tasks` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Tasks`
+(
+  `id` int
+(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  `user_id` int
+(11) NOT NULL,
+  PRIMARY KEY
+(`id`),
+  UNIQUE KEY `id_UNIQUE`
+(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -71,12 +80,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `Users` (
+CREATE TABLE `Users`
+(
   `name` text NOT NULL,
-  `id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  `id` int
+(11) NOT NULL,
+  PRIMARY KEY
+(`id`),
+  UNIQUE KEY `id_UNIQUE`
+(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
